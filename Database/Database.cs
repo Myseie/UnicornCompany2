1ba7d4ca73fe4b219b8561bdfa1ec8b4b5d8756d
Myseie <emil_westerholm@hotmail.com>
2023-02-18 13:37:34 +0100

    Initial commit

---
+++ Database/Database.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Database
+    {
+        private IMongoDatabase GetDb()
+        {
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("EmployeesDB");
+            return db;
+        }
+
+
+        public async Task SaveEmployee (Employee employee)
+        {
+            await GetDb().GetCollection<Employee>("Employee")
+                .InsertOneAsync(employee);
+        }
+
+        public async Task <List<Employee>> GetAllEmployees()
+        {
+            var employees = await GetDb().GetCollection<Employee>("Employees")
+                .Find(b=>true)
+                .ToListAsync();
+            return employees;
+        }
+
+        public async Task<Employee> GetEmployeeById(string id)
+        {
+            ObjectId _id = new ObjectId(id);
+
+            var employee = await GetDb().GetCollection<Employee>("Employees")
+                .Find(b=>b.Id==_id)
+                .SingleOrDefaultAsync();
+            return employee;
+        }
+    }
+}
---
+++ UnicornCompany2/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Database;
+
+namespace UnicornCompany2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public async Task <IActionResult> Index()
+        {
+            Database.Database db = new Database.Database();
+            List <Employee> employees = await db.GetAllEmployees();
+            return View(employees);
+        }
+
+
+        
+    }
+}
